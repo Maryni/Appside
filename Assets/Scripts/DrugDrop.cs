84a99e62e384257d5323ee0aa5fc064f0702b949
Maryni <37656934+Maryni@users.noreply.github.com>
2021-11-22 18:49:27 +0200

    add animations, add DrugDrop and Movement for character when player drup joystick

---
+++ Assets/Scripts/DrugDrop.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DrugDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    [SerializeField] private RectTransform rectTransform;
+    [SerializeField] private Vector2 startTransform;
+    [SerializeField] private Vector2 endTransform;
+
+    private void OnValidate()
+    {
+        if (rectTransform == null)
+        {
+            rectTransform = GetComponent<RectTransform>();
+        }
+    }
+
+    private void Start()
+    {
+        startTransform = rectTransform.anchoredPosition;
+        endTransform = Vector2.zero;
+    }
+
+    public Vector2 GetStartPosition()
+    {
+        if (startTransform != Vector2.zero)
+        {
+            return startTransform;
+        }
+        else
+            return Vector2.zero;
+    }
+
+    public Vector2 GetEndPosition()
+    {
+        if (endTransform != Vector2.zero)
+        {
+            return endTransform;
+        }
+        else
+            return Vector2.zero;
+    }
+
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("OnPointDown");
+    }
+
+    void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrug");
+    }
+
+    void IDragHandler.OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnDrug");
+        rectTransform.anchoredPosition += eventData.delta;
+        endTransform = rectTransform.anchoredPosition;
+    }
+
+    void IEndDragHandler.OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrug");
+        endTransform = rectTransform.anchoredPosition;
+        rectTransform.anchoredPosition = startTransform;
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+    [SerializeField] private Animator animator;
+    [SerializeField] private Vector3 startPos;
+    [SerializeField] private Vector3 endPos;
+    [SerializeField] private Vector3 direction;
+    [SerializeField] private DrugDrop drugDrop;
+    [SerializeField] private GameObject player;
+    [SerializeField] private Rigidbody rig;
+
+    private void OnValidate()
+    {
+        if (player == null)
+        {
+            player = gameObject;
+        }
+    }
+
+    private void Start()
+    {
+        cam = FindObjectOfType<Camera>();
+        animator.SetBool("isWalking", true);
+        rig = player.GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        GetPosition();
+    }
+
+    private void GetPosition()
+    {
+        startPos = drugDrop.GetStartPosition();
+        endPos = drugDrop.GetEndPosition();
+        Move();
+    }
+
+    private void Move()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            direction = new Vector3(endPos.x - startPos.x, 0, endPos.y - startPos.y).normalized;
+            rig.velocity = direction;
+            direction = Vector2.zero;
+        }
+    }
+}
